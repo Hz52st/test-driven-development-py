a5594f955f60ea6f9614241f3b43889808f4a1b9
stephenkung <istephenkung@outlook.com>
2019-01-14 11:09:11 +0800

    使用模板文件生成前端html文件

--- functional_tests.py
+++ functional_tests.py
@@ -18,6 +18,7 @@ class NewVisitorTest(unittest.TestCase):
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
 
+		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(
 			inputbox.get_attribute('placeholder'),
 			'Enter a to-do item'
@@ -30,7 +31,8 @@ class NewVisitorTest(unittest.TestCase):
 		rows = table.find_elements_by_tag_name('tr')
 
 		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
+			any(row.text == '1: Buy peacock feathers' for row in rows),
+			'New to-do item did not appear in table'
 		)
 		self.fail("Finish the test!")
 
