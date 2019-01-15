5814655cfb212b9f25b07797b8874f74d58c6fdc
stephenkung <istephenkung@outlook.com>
2019-01-15 11:21:41 +0800

    POST请求后进行重定向，并在模板中显示所有输入项

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,20 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template.context_processors import csrf
 from django.views.decorators.csrf import ensure_csrf_cookie
 
+from lists.models import Item
 
 # Create your views here.
 
 # @ensure_csrf_cookie
 def home_page(request):
 	if request.method == 'POST':
-		# return HttpResponse(request.POST['item_text'])
-		return render(
-			request, 
-			'home.html', 
-			{
-				'new_item_text': request.POST.get('item_text', ''),
-			},
-		)
-	return render(request, 'home.html')
+		new_item_text = request.POST.get('item_text', '')
+		Item.objects.create(text=new_item_text)
+		return redirect('/')
+	items = Item.objects.all()
+	return render(
+		request, 'home.html', {'items': items,},
+	)
