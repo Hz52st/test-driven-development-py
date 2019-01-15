f611fe17b0a0f4fd231c1324ba462cb05fa4a49e
stephenkung <istephenkung@outlook.com>
2019-01-15 15:23:17 +0800

    提出check_row_in_list_table函数

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -15,6 +15,12 @@ class NewVisitorTest(LiveServerTestCase):
 		if self.browser:
 			self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get(self.live_server_url)
 		self.assertIn('To-Do', self.browser.title)
@@ -29,15 +35,16 @@ class NewVisitorTest(LiveServerTestCase):
 
 		inputbox.send_keys('Buy peacock feathers')
 		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
 		edith_list_url = self.browser.current_url
 		self.assertRegex(edith_list_url, '/lists/.+')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
-		time.sleep(3)
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(3)
+		time.sleep(1)
 
 		table = self.browser.find_element_by_id('id_list_table')
 
@@ -67,16 +74,17 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox = self.send_keys('Buy milk')
 		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
 
 		francis_list_url = self.browser.current_url
-		self.assertRegex(francis_list_url, '/list/.+')
+		self.assertRegex(francis_list_url, '/lists/.+')
 		self.assertNotEqual(francis_list_url, edith_list_url)
 
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertIn('Buy milk', page_text)
 
-		
+
 
 
 
