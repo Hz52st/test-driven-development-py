6c4924252198d6b0da07d0bf843a1c286f9aa439
stephenkung <istephenkung@outlook.com>
2019-01-15 10:42:11 +0800

    添加Item模型，并生成migrtions文件

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,8 +3,10 @@ from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.template import RequestContext
+
 import re
 
+from lists.models import Item
 
 
 from lists.views import home_page
@@ -73,6 +75,25 @@ def remove_csrf_tag(text):
 	去除 HTML 中的 CSRF line 的 function：
     """
     return re.sub(r'<[^>]*csrfmiddlewaretoken[^>]*>', '', text)
+
+
+class ItemModelTest(TestCase):
+
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(second_saved_item.text, 'Item the second')
 
 
 
