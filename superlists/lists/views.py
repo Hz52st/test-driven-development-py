e0ac44b8ebc573e0322e4f828c5a3b5f9be38162
stephenkung <istephenkung@outlook.com>
2019-01-16 10:58:17 +0800

    添加新的路由和师徒用于添加已存在的list

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -15,12 +15,17 @@ def new_list(request):
 	new_item_text = request.POST.get('item_text', '')
 	list_ =List.objects.create()
 	Item.objects.create(text=new_item_text, list=list_)
-	return redirect('/lists/the-only-list-in-the-world/')
+	return redirect('/lists/%d/' % list_.id)
 
 
-def view_list(request):
-	items = Item.objects.all()
+def view_list(request, list_id):
+	list_ = List.objects.get(id=list_id)
 	return render(
-		request, 'list.html', {'items': items}
+		request, 'list.html', {'list': list_}
 	)
 
+def add_item(request, list_id):
+	new_item_text = request.POST.get('item_text', '')
+	list_ =List.objects.get(id=list_id)
+	Item.objects.create(text=new_item_text, list=list_)
+	return redirect('/lists/%d/' % list_.id)
